d923d74a0c92d33b4990e44282114bc902039684
DhananjayNarayan <dhananjayashwath@gmail.com>
2022-07-23 22:45:25 -0400

    feat: Added a function to calculate power of two values

--- src/F5.java
+++ src/F5.java
@@ -1,6 +1,11 @@
 
 class f5{
 
+	public int powerCalc(int a,int b) {
+		int res = a^b;
+		return res;
+	}
+	
     public static void main(String args[]){
     	System.out.println("In Eclipse Now: ");
         System.out.println("F5: ab^x");
