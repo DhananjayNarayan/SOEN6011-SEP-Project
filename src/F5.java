a220f7dc7e6224233bed02c886fafed2965913c5
DhananjayNarayan <dhananjayashwath@gmail.com>
2022-07-23 22:51:29 -0400

    feat: Handled input and parameter passing to the power function

--- src/F5.java
+++ src/F5.java
@@ -1,7 +1,8 @@
+import java.util.*;
 
 class f5{
 
-	public int powerCalc(int a,int b) {
+	public static int powerCalc(int a,int b) {
 		int res = a^b;
 		return res;
 	}
@@ -10,5 +11,17 @@ class f5{
     	System.out.println("In Eclipse Now: ");
         System.out.println("F5: ab^x");
         System.out.println("In Source Now");
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the values of a,b and x");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int x =  sc.nextInt();
+        
+        int bx = powerCalc(b,x);
+        int answer = a*bx;
+        
+        System.out.println("Result is : "+answer );
+        
     }
 }
