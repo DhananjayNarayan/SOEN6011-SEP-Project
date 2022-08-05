df67166a7e70024afa36fddb497ade9ed4d4afd7
DhananjayNarayan <dhananjayashwath@gmail.com>
2022-08-05 02:33:55 -0400

    feat: added a function to calculate exponent recursively

--- src/F5.java
+++ src/F5.java
@@ -4,7 +4,7 @@ import java.util.*;
 /**
  * The main class f5 having main function to start the execution. Contains functions for calculations. 
  * @author Dhananjay Narayan
- * v 0.0.3
+ * v 0.0.4
  *
  */
 class f5{
@@ -15,16 +15,34 @@ class f5{
 	 * @param b the power to which the base will be raised
 	 * @return res the result of the power calculation.
 	 */
-	public static double calculateExponential(double a,double b,int x) throws Exception{
+	public static double calculateExponentialResult(double a,double b,int x) throws Exception{
 
-
+        double bx =0;
 		if (x == 0) {
+			return 1.0;
+		}
+		else {
+			 bx = calculateExponentRecursive(b,x);
+			
+		}
+		double result = a*bx;
+		return result;
+	}
+
+	private static double calculateExponentRecursive(double b, int x) {
+		// TODO Auto-generated method stub
+		if(x==0) {
 			return 1;
 		}
 		else {
-			double res = b*x;
-			return res;
+			return b*calculateExponentRecursive(b,x-1);
 		}
+		
+	}
+	
+	private static double calculateExponenetIterative(double b,int x) {
+		double result = 0;
+		return result;
 	}
 
 	/**
@@ -32,11 +50,15 @@ class f5{
 	 * @param args
 	 */
 	public static void main(String args[]){
-		
-		System.out.println("F5: ab^x");
-		System.out.println("In Source Now");
-
 		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("**************************");
+		System.out.println("**********ETERNITY********");
+		System.out.println("**************************");
+		System.out.println("A Calculator to calculate F5: ab^x");
+
+
+		
 		System.out.println("Enter the values of a,b and x");
 		double a = sc.nextDouble();
 		double b = sc.nextDouble();
@@ -44,7 +66,8 @@ class f5{
 
 		
 		try {
-			double bx = calculateExponential(a,b,x);
+			double bx = calculateExponentialResult(a,b,x);
+			System.out.println(bx);
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			System.out.println("Incorrect Input");
